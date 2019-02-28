c4764adb465985d5a4466f82738c8ac07c43a1d0
cleverNathalia <danielledramaqueeeen@gmail.com>
2019-02-28 20:08:13 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,267 @@
+
+$(document).ready(function () {
+    $('#shopperLayout').append(
+
+        '<div class="container-fluid">' +
+        ' <div class="row">' +
+        '<div class="col-12 centerText paddingSmallHeader futuraExtraHeavy">' +
+        ' <h6>' +
+        '  LOREM IPSUM DOLOR SIT AMET' +
+        '   </h6>' +
+        ' </div>' +
+        '<div class="col-12 centerText paddingSentence futuraLight">' +
+        ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit vestibulum metus, sit amet condimentum sapien vestibulum' +
+        ' in. Curabitur sit amet magna sed nisi elementum semper sed a quam. Proin dignissim tincidunt imperdiet. Ut' +
+        ' enim enim, auctor et libero quis, volutpat finibus purus. Etiam faucibus, nibh id consectetur placerat, arcu' +
+        ' eros dapibus ipsum, in lacinia tortor odio a nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
+        '  Aenean hendrerit vestibulum metus, sit amet condimentum sapien vestibulum in. Curabitur sit amet magna sed' +
+        'nisi elementum semper sed a quam. Proin dignissim tincidunt imperdiet. Ut enim enim, auctor et libero quis,' +
+        'volutpat finibus purus. Etiam faucibus, nibh id consectetur placerat, arcu eros dapibus ipsum, in lacinia' +
+        ' tortor odio a nisl.' +
+        '  </div>' +
+        '</div>' +
+
+        '  <div class="row marginBlock">' +
+        '<div class="col-4 centerText">' +
+        '  <i class="fas fa-globe-americas bigIcons boxIcon"></i>' +
+        ' </div>' +
+        '<div class="col-8">' +
+        '<h6 class=" paddingSmallHeaderOther futuraExtraHeavy">' +
+        'LOREM IPSUM DOLOR SIT AMET CONSECTETUR ADIPISCING ELIT' +
+        '   </h6>' +
+        '<div class=" paddingSentence futuraLight ">' +
+        '<br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit vestibulum metus, sit amet' +
+        ' condimentum sapien vestibulum in. Curabitur sit amet magna sed nisi elementum semper sed a quam.' +
+        '    </div>' +
+        '</div>' +
+        ' </div>' +
+        '</div>' +
+        '<div class="container-fluid">' +
+        '<div class="row">' +
+        ' <div class="col-12 centerText paddingSmallHeader futuraExtraHeavy">' +
+        '<h6>' +
+        'TRACK ORDER' +
+        '</h6>' +
+        ' </div>' +
+        '<div class="col-12 centerText paddingSentence futuraLight">' +
+        ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit vestibulum metus, sit amet condimentum sapien vestibulum' +
+        '   in. Curabitur sit amet magna sed nisi elementum semper sed a quam. Proin dignissim tincidunt imperdiet. Ut' +
+        'enim enim, auctor et libero quis, volutpat finibus purus. Etiam faucibus, nibh id consectetur placerat, arcu' +
+        ' eros dapibus ipsum, in lacinia tortor odio a nisl.' +
+        '  </div>' +
+        ' </div>' +
+        '     </div>' +
+
+        '<div class="container-fluid">' +
+        ' <div class="row marginBlock">' +
+        '<div class="col-3 centerText ">' +
+        '  <div class="progress-circle p30">' +
+        '<span>' +
+        '<i class="fas fa-boxes icons insideCircle"></i>' +
+        '       </span>' +
+        ' <div class="left-half-clipper">' +
+        '<div class="first50-bar"></div>' +
+        ' <div class="value-bar"></div>' +
+        '  </div>' +
+        ' </div>' +
+        ' </div>' +
+        '<div class="col-lg-5 col-md-5 col-sm-12">' +
+        ' <h6 class="leftText futuraExtraHeavy">' +
+        '  ORDER PROGRESS' +
+        '</h6>' +
+        ' <div class="row">' +
+        '  <div class="col-12 futuraLight orderNum">' +
+        '    Order Number: 7894hgjfnes23' +
+        '  </div>' +
+        '  </div>' +
+        '  <div class="row centerText">' +
+        '  <div class="col-6 futuraLight remaining">' +
+        '  <nav aria-label="breadcrumb">' +
+        ' <ol class="breadcrumb">' +
+        '  <li class="breadcrumb-item active" aria-current="page">' +
+        '<b>3 Days Remaining</b>' +
+        '      </li>' +
+        ' </ol>' +
+        '  </nav>' +
+        ' </div>' +
+        '<div class="col-6 futuraLight courier">' +
+        '  <nav aria-label="breadcrumb">' +
+        ' <ol class="breadcrumb">' +
+        ' <li class="breadcrumb-item active" aria-current="page">' +
+        ' <b>With Courier</b>' +
+        '  </li>' +
+        '</ol>' +
+        ' </nav>' +
+        '  </div>' +
+        ' </div>' +
+        '  </div>' +
+        '<div class="col-4 centerText futuraHeavy">' +
+        '<a href="details.html">' +
+        ' <button class="btn btn-primary infoBtn" type="button" >MORE' +
+        ' <br>INFO</button>' +
+        '</a>' +
+        ' </div>' +
+        ' </div>' +
+        '  <div class="row marginBlock">' +
+        '   <div class="col-3 centerText ">' +
+        ' <div class="progress-circle over50 p60">' +
+        '<span>' +
+        '<i class="fas fa-boxes icons insideCircle"></i>' +
+        '</span>' +
+        '<div class="left-half-clipper">' +
+        ' <div class="first50-bar"></div>' +
+        ' <div class="value-bar"></div>' +
+        '</div>' +
+        ' </div>' +
+        ' </div>' +
+        '<div class="col-lg-5 col-md-5 col-sm-12">' +
+        '  <h6 class="leftText futuraExtraHeavy">' +
+        ' ORDER PROGRESS' +
+        ' </h6>' +
+        '<div class="row">' +
+        '  <div class="col-12 futuraLight orderNum">' +
+        'Order Number: 7894hgjfnes23' +
+        ' </div>' +
+        '</div>' +
+        ' <div class="row centerText">' +
+        ' <div class="col-6 futuraLight remaining">' +
+        '<nav aria-label="breadcrumb">' +
+        ' <ol class="breadcrumb">' +
+        '<li class="breadcrumb-item active" aria-current="page">' +
+        ' <b>4 Days Remaining</b>' +
+        ' </li>' +
+        ' </ol>' +
+        '</nav>' +
+        ' </div>' +
+        ' <div class="col-6 futuraLight courier">' +
+        ' <nav aria-label="breadcrumb">' +
+        '<ol class="breadcrumb">' +
+        '  <li class="breadcrumb-item active" aria-current="page">' +
+        ' <b>With Courier</b>' +
+        '  </li>' +
+        '  </ol>' +
+        ' </nav>' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '<div class="col-4 centerText futuraHeavy">' +
+        '</div>' +
+        ' </div>' +
+        '     </div>'
+    );
+
+    $('#businessLayout').append(
+        '<div class="container-fluid">' +
+        ' <div class="row">' +
+        '<div class="col-12 centerText paddingSmallHeader futuraExtraHeavy">' +
+        '<h6>TRACK ORDER</h6>' +
+        '</div>' +
+        '<div class="col-12 centerText paddingSentence futuraLight">' +
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit vestibulum metus, sit amet condimentum sapien vestibulum' +
+        '                in. Curabitur sit amet magna sed nisi elementum semper sed a quam. Proin dignissim tincidunt imperdiet. Ut' +
+        'enim enim, auctor et libero quis, volutpat finibus purus. Etiam faucibus, nibh id consectetur placerat, arcu' +
+        ' eros dapibus ipsum, in lacinia tortor odio a nisl.' +
+        '</div>' +
+        '</div>' +
+        '<div class="row">' +
+        '<div class="col-4 centerText ">' +
+        ' <i class="fas fa-box icons boxIcon"></i>' +
+        '</div>' +
+        ' <div class="col-8">' +
+        '<h6 class=" paddingSmallHeaderOther futuraExtraHeavy">' +
+        '  ORDER PROGRESS' +
+        '</h6>' +
+        ' <div class=" paddingSentence futuraLight ">' +
+        ' Order Number: 7894hgjfnes23' +
+        '<br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit vestibulum metus, sit amet' +
+        '  condimentum sapien vestibulum in. Curabitur sit amet magna sed nisi elementum semper sed a quam.' +
+        '</div>' +
+        '    </div>' +
+        ' </div>' +
+        '</div>' +
+        '<div class="container progressBar">' +
+        '<div class="progress">' +
+        '<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0"' +
+        'aria-valuemax="100" style="width: 26%">' +
+        ' 26%' +
+        ' </div>' +
+        '</div>' +
+        ' </div>' +
+        '<div class="container-fluid">' +
+        ' <div class="row">' +
+        '<div class="col-6 centerText futuraLight">' +
+        '   <b>3 Days Remaining</b>' +
+        '   </div>' +
+        ' <div class="col-6 centerText futuraLight">' +
+        ' <b>Currently: With Courier</b>' +
+        '</div>' +
+        '  </div>' +
+        '</div>' +
+        '<div class="container-fluid lastBlocks">' +
+        '   <div class="row rowMargin">' +
+        '  <div class="col-4 centerText ">' +
+        ' <i class="fas fa-map-marked-alt icons boxIcon"></i>' +
+        '  </div>' +
+        ' <div class="col-8">' +
+        '   <h6 class=" paddingSmallHeaderOther futuraExtraHeavy">' +
+        ' LOREM IPSUM DOLOR SIT AMET' +
+        '    </h6>' +
+        '  <div class=" paddingSentence futuraLight">' +
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit vestibulum metus, sit amet condimentum sapien vestibulum' +
+        '                    in. Curabitur sit amet magna sed nisi elementum semper sed a quam.' +
+        ' </div>' +
+        '         </div>' +
+        ' </div>' +
+        ' <div class="row rowMargin">' +
+        '<div class="col-4 centerText ">' +
+        '<i class="fas fa-male icons boxIcon"></i>' +
+        '     </div>' +
+        ' <div class="col-8">' +
+        '   <h6 class=" paddingSmallHeaderOther futuraExtraHeavy">' +
+        '  LOREM IPSUM DOLOR SIT AMET' +
+        ' </h6>' +
+        '<div class=" paddingSentence futuraLight">' +
+        '  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit vestibulum metus, sit amet condimentum sapien vestibulum' +
+        ' in. Curabitur sit amet magna sed nisi elementum semper sed a quam.' +
+        ' </div>' +
+        '         </div>' +
+        '  </div>' +
+        '   <div class="row rowMargin">' +
+        '  <div class="col-4 centerText ">' +
+        ' <i class="fas fa-check-square icons boxIcon"></i>' +
+        '</div>' +
+        ' <div class="col-8">' +
+        ' <div class=" futuraLight paddingOneSentence">' +
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit vestibulum metus, sit amet condimentum sapien vestibulum' +
+        '  in. Curabitur sit amet magna sed nisi elementum semper sed a quam.' +
+        '</div>' +
+        '</div>' +
+        '</div>' +
+        '</div>'
+    );
+
+    $('#businessLayout').hide();
+    $('#shopperLayout').hide();
+});
+
+function imShopper() {
+    $('#shopper').addClass("addChoiceClick");
+    $('#business').removeClass("addChoiceClick");
+
+    $('#shopperLayout').show();
+    $('#businessLayout').hide();
+    $('#choices').hide();
+
+
+    $('html, body').animate({ scrollTop: $("#shopperLayout").offset().top }, 1200);
+}
+function imBusiness() {
+    $('#business').addClass("addChoiceClick");
+    $('#shopper').removeClass("addChoiceClick");
+
+    $('#businessLayout').show();
+    $('#shopperLayout').hide();
+    $('#choices').hide();
+
+    $('html, body').animate({ scrollTop: $("#businessLayout").offset().top }, 1200);
+}
